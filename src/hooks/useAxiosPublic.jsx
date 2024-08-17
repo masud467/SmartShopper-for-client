76176b51ea201ece5ebd98a38c80cb70983832c9
masud4657 <masudpfrnt@gmail.com>
2024-08-17 17:27:25 +0600

    Added the Project Title and favicon in index.html file.

--- src/hooks/useAxiosPublic.jsx
+++ src/hooks/useAxiosPublic.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const axiosPublic= axios.create({
-    baseURL:'http://localhost:6001'
+    baseURL:'https://smart-shopper-for-server.vercel.app/'
 })
 const useAxiosPublic = () => {
     return axiosPublic
